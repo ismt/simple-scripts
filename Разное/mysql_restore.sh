69fab94c7f1620b66752e64514c48707666c72c6
Иванов Стас <ivanov.stanislav@stemsc.com>
2018-07-31 13:21:07 +0300

    ...

--- Разное/mysql_restore.sh
+++ Разное/mysql_restore.sh
@@ -1,4 +1,6 @@
-pv -N file ./t.sql.gz | unpigz - | pv -cN mysql |  mysql --user=root \
+#!/bin/bash
+
+pv -N file ./t.sql.xz | xz  -d  --threads=0 | pv -cN mysql |  mysql --user=root \
                                                           --password=test \
                                                           --host=127.0.0.1 \
                                                           --max_allowed_packet=1000M \
