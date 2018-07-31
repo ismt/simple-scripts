f2218240f8c8d5e139b4f336a386f43b40c405fb
Иванов Стас <ivanov.stanislav@stemsc.com>
2018-07-31 17:24:39 +0300

    ...

--- Разное/mysql_dump.sh
+++ Разное/mysql_dump.sh
@@ -3,10 +3,14 @@
 
 arc="pv -cN mysqldump | xz -1 -c  --threads=0 | pv -cN xz > $(uuidgen  -t).sql.xz"
 
+declare -A  server
+server=( [user]=root [password]=test [host]=127.0.0.1)
+
+
 mysqldump \
-    --user=root \
-    --password=test \
-    --host=127.0.0.1 \
+    --user=${server[user]} \
+    --password=${server[password]} \
+    --host=${server[host]} \
     --max_allowed_packet=1000M \
     --add-locks \
     --extended-insert \
--- Разное/mysql_restore.sh
+++ Разное/mysql_restore.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-pv -N file ./t.sql.xz | xz  -d  --threads=0 | pv -cN mysql |  mysql --user=root \
-                                                          --password=test \
-                                                          --host=127.0.0.1 \
-                                                          --max_allowed_packet=1000M \
-                                                          --database=test
+declare -A  server
+server=( [user]=root [password]=test [host]=127.0.0.1)
 
-
-
-
-
-
-
-
-
-
+pv -N file ./t.sql.xz | xzcat  --threads=0 | pv -cN mysql |  \
+mysql --user=${server[user]} \
+      --password=${server[password]} \
+      --host=${server[host]}
+      --max_allowed_packet=1000M \
+      --database=test
 
