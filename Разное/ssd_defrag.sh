d4fcddf10714d0906c08ab15c0d3385ff8d9ac13
Иванов Стас <ivanov.stanislav@stemsc.com>
2019-12-15 23:07:02 +0200

    ...

--- Разное/ssd_defrag.sh
+++ Разное/ssd_defrag.sh
@@ -19,9 +19,6 @@ if [[ -n "$exists_ext4" ]]
 then
     e4defrag "$device" > /dev/null
 
-    # Принудительно каждый файл, команда выше работает не до конца
-    # find / -xdev -type f -exec e4defrag -- "{}" \; > /dev/null
-
 elif [[ -n "$exists_xfs" ]]
 then
     xfs_fsr "$device" > /dev/null
