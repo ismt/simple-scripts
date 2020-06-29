f94c5d66eef6b935324d7ae3d616534382f6704f
Иванов Стас <ivanov.stanislav@stemsc.com>
2020-06-30 02:56:39 +0300

    ...

--- Разное/django/install_django_server_centos_7.sh
+++ Разное/django/install_django_server_centos_7.sh
@@ -7,7 +7,9 @@ yum -y install https://repo.ius.io/ius-release-el7.rpm https://dl.fedoraproject.
 
 yum -y install mc wget libreoffice-calc htop iotop atop zstd zip lz4 pbzip2 p7zip lzop pv git screen unzip pigz openssh-server
 
-yum -y install python36u python36u-devel python36u-pip net-tools gcc kernel-devel psmisc libxml2-devel libxslt-devel libmemcached-devel nginx npm httpd-tools memcached mysqlreport perl-DBD-MySQL smartmontools jpegoptim optipng redis40u hdparm phoronix-test-suite fio
+yum -y install python36u python36u-devel python36u-pip net-tools gcc kernel-devel psmisc \
+               libxml2-devel libxslt-devel libmemcached-devel nginx npm httpd-tools memcached \
+               mysqlreport perl-DBD-MySQL smartmontools jpegoptim optipng redis40u hdparm phoronix-test-suite fio ncdu
 
 
 yum -y erase mariadb*
