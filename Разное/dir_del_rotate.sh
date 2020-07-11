26958569d296b133f3284a3d699fc76dde8388ca
Иванов Стас <ivanov.stanislav@stemsc.com>
2020-07-12 01:07:03 +0300

    ...

--- Разное/dir_del_rotate.sh
+++ Разное/dir_del_rotate.sh
@@ -6,7 +6,7 @@ function CleanDir {
   base_dir=$1
 
   start_base_dir=$(echo ${base_dir:0:${#operate_with_dir}})
-  if [ "$start_base_dir" != "$operate_with_dir" ]
+  if [[ "$start_base_dir" != "$operate_with_dir" ]]
   then
     echo Нельзя, можно только в директории $operate_with_dir
     exit
@@ -17,12 +17,12 @@ function CleanDir {
   count=0
   let "count_res = $count_dirs - $max_dirs"
   cd "$base_dir"
-  echo Количество директорий $count_dirs , удалить $count_res старых в $base_dir
+  echo Количество директорий ${count_dirs} , удалить ${count_res} старых в ${base_dir}
   ls -t -r "$base_dir" | while read file
   do
-    if [ $count -lt $count_res ]
+    if [[ ${count} -lt ${count_res} ]]
     then
-      if [ -d "$base_dir/$file" ]
+      if [[ -d "$base_dir/$file" ]]
       then
         echo Директория, удаляем "$base_dir/$file"
         rm -f -r "$base_dir/$file"
