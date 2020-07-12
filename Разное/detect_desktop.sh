0a76280b1c3384b3caed994ec406dd1638320986
Иванов Стас <ivanov.stanislav@stemsc.com>
2020-07-12 18:26:37 +0300

    ...

--- Разное/detect_desktop.sh
+++ Разное/detect_desktop.sh
@@ -6,64 +6,69 @@ detectDE()
     unset GREP_OPTIONS
 
     if [[ -n "${XDG_CURRENT_DESKTOP}" ]]; then
-      case "${XDG_CURRENT_DESKTOP}" in
-         GNOME)
-           DE=gnome;
-           ;;
-         KDE)
-           DE=kde;
-           ;;
-         LXDE)
-           DE=lxde;
-           ;;
-         XFCE)
-           DE=xfce
-           ;;
-         Unity|UNITY|unity)
-           DE=unity;
-           ;;
-      esac
+        case "${XDG_CURRENT_DESKTOP}" in
+            GNOME)
+                DE=gnome;
+            ;;
+            KDE)
+                DE=kde;
+            ;;
+            LXDE)
+                DE=lxde;
+            ;;
+            XFCE)
+                DE=xfce
+            ;;
+            Unity | UNITY | unity)
+                DE=unity;
+            ;;
+        esac
     fi
-    
+
     if [[ x"$DE" = x"" ]]; then
-      # classic fallbacks
-      if [[ x"$KDE_FULL_SESSION" = x"true" ]]; then DE=kde;
-      elif [[ x"$GNOME_DESKTOP_SESSION_ID" != x"" ]]; then DE=gnome;
-      elif `dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.GetNameOwner string:org.gnome.SessionManager > /dev/null 2>&1` ; then DE=gnome;
-      elif xprop -root _DT_SAVE_MODE 2> /dev/null | grep ' = \"xfce4\"$' >/dev/null 2>&1; then DE=xfce;
-      elif xprop -root 2> /dev/null | grep -i '^xfce_desktop_window' >/dev/null 2>&1; then DE=xfce
-      fi
+        # classic fallbacks
+        if [[ x"$KDE_FULL_SESSION" = x"true" ]]; then
+            DE=kde;
+        elif [[ x"$GNOME_DESKTOP_SESSION_ID" != x"" ]]; then
+            DE=gnome;
+        elif `dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.GetNameOwner string:org.gnome.SessionManager > /dev/null 2>&1`; then
+            DE=gnome;
+        elif xprop -root _DT_SAVE_MODE 2> /dev/null | grep ' = \"xfce4\"$' > /dev/null 2>&1; then
+            DE=xfce;
+        elif xprop -root 2> /dev/null | grep -i '^xfce_desktop_window' > /dev/null 2>&1; then
+            DE=xfce
+        fi
     fi
-    
+
     if [[ x"$DE" = x"" ]]; then
-      # fallback to checking $DESKTOP_SESSION
-      case "$DESKTOP_SESSION" in
-         gnome)
-           DE=gnome;
-           ;;
-         LXDE)
-           DE=lxde; 
-           ;;
-         xfce|xfce4)
-           DE=xfce;
-           ;;
-      esac
+        # fallback to checking $DESKTOP_SESSION
+        case "$DESKTOP_SESSION" in
+            gnome)
+                DE=gnome;
+            ;;
+            LXDE)
+                DE=lxde;
+            ;;
+            xfce | xfce4)
+                DE=xfce;
+            ;;
+        esac
     fi
 
     if [[ x"$DE" = x"" ]]; then
 
-      # fallback to uname output for other platforms
-      case "$(uname 2>/dev/null)" in 
-        Darwin)
-          DE=darwin;
-          ;;
-      esac
+        # fallback to uname output for other platforms
+        case "$(uname 2> /dev/null)" in
+            Darwin)
+                DE=darwin;
+            ;;
+        esac
     fi
-    
+
     if [[ x"$DE" = x"gnome" ]]; then
-      # gnome-default-applications-properties is only available in GNOME 2.x
-      # but not in GNOME 3.x
-      which gnome-default-applications-properties > /dev/null 2>&1  || DE="gnome3"
+        # gnome-default-applications-properties is only available in GNOME 2.x
+        # but not in GNOME 3.x
+        which gnome-default-applications-properties > /dev/null 2>&1 || DE="gnome3"
     fi
     echo ${DE}
 }
--- Разное/django/install_django_server_centos_7.sh
+++ Разное/django/install_django_server_centos_7.sh
@@ -4,52 +4,52 @@
 yum -y install dialog
 
 
-retval=$(dialog  \
-        --title 'Настройка хоста'  \
-        --menu 'Выбор' 15 50 3 \
-            1 'Установить пакеты' \
-            2 'Старт вебсервисов'   --output-fd 1
-        )
-            
+retval=$(dialog --output-fd 1  \
+ --title 'Настройка хоста'  \
+ --menu 'Выбор' 15 50 3  \
+ 1 'Установить пакеты'  \
+ 2 'Старт вебсервисов'  \
+)
+
 case ${retval} in
-1)
-    yum -y install https://repo.ius.io/ius-release-el7.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    1)
+        yum -y install https://repo.ius.io/ius-release-el7.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-    yum -y install mc wget libreoffice-calc htop iotop atop zstd zip lz4 pbzip2 p7zip lzop pv git screen unzip pigz openssh-server
+        yum -y install mc wget libreoffice-calc htop iotop atop zstd zip lz4 pbzip2 p7zip lzop pv git screen unzip pigz openssh-server
 
-    yum -y install python36u python36u-devel python36u-pip net-tools gcc kernel-devel psmisc \
-                   libxml2-devel libxslt-devel libmemcached-devel nginx npm httpd-tools memcached \
-                   mysqlreport perl-DBD-MySQL smartmontools jpegoptim optipng redis40u hdparm phoronix-test-suite fio ncdu
-
-
-    yum -y erase mariadb*
-
-    curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
-
-    yum -y install MariaDB-server MariaDB-shared MariaDB-devel MariaDB-rocksdb-engine MariaDB-tokudb-engine
-
-    yum  -y  install  http://sphinxsearch.com/files/sphinx-2.2.11-1.rhel7.x86_64.rpm
-
-    yum update -y
-
-   ;;
+        yum -y install python36u python36u-devel python36u-pip net-tools gcc kernel-devel psmisc  \
+ libxml2-devel libxslt-devel libmemcached-devel nginx npm httpd-tools memcached  \
+ mysqlreport perl-DBD-MySQL smartmontools jpegoptim optipng redis40u hdparm phoronix-test-suite fio ncdu
 
 
-2)
-    pip3.6 install pip --upgrade
+        yum -y erase mariadb*
 
-    pip3.6 install uwsgi virtualenv pipenv --upgrade
+        curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash
 
-    systemctl start searchd
-    systemctl enable searchd
+        yum -y install MariaDB-server MariaDB-shared MariaDB-devel MariaDB-rocksdb-engine MariaDB-tokudb-engine
 
-    systemctl start mariadb
-    systemctl enable mariadb
+        yum -y install http://sphinxsearch.com/files/sphinx-2.2.11-1.rhel7.x86_64.rpm
 
-    systemctl start redis
-    systemctl enable redis
+        yum update -y
 
-   ;;
+    ;;
+
+
+    2)
+        pip3.6 install pip --upgrade
+
+        pip3.6 install uwsgi virtualenv pipenv --upgrade
+
+        systemctl start searchd
+        systemctl enable searchd
+
+        systemctl start mariadb
+        systemctl enable mariadb
+
+        systemctl start redis
+        systemctl enable redis
+
+    ;;
 esac
 
 
